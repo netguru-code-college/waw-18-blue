8e43b42d4a3f9e982c6df84416a2f2f19bb97eca
Janek Kamyk <jan.kamyk@netguru.pl>
2018-09-15 14:55:30 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,16 +29,6 @@ ActiveRecord::Schema.define(version: 2018_09_15_120053) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table "addresses", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "location_id"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["location_id"], name: "index_addresses_on_location_id"
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
-
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -100,8 +90,6 @@ ActiveRecord::Schema.define(version: 2018_09_15_120053) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "addresses", "locations"
-  add_foreign_key "addresses", "users"
   add_foreign_key "events", "locations"
   add_foreign_key "ratings", "locations"
   add_foreign_key "ratings", "users"
