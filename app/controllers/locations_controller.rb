80ef7ffeb4b51c9c9e6c9d2368d14b44e41e5016
Dzonyy <paszcza.jakub@gmail.com>
2018-09-15 16:56:56 +0200

    locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,50 @@
 class LocationsController < ApplicationController
   def index
+    @locations = Location.all.order("created_at DESC")
   end
 
   def new
+    @location = Location.new
+  end
+
+  def show
+    @location = Location.find(params[:id])
   end
 
   def create
-  end
+    @location = Location.new(location_params)
 
-  def edit
+    if @location.save
+        redirect_to action: "index"
+    else
+      render 'new'
+    end
   end
 
   def update
+    @location = Location.find(params[:id])
+
+    if @location.update(location_params)
+      redirect_to @location
+    else
+      render 'edit'
+    end
+  end
+
+  def edit 
+    @location = Location.find(params[:id])
   end
 
   def destroy
+    @Location = Location.find(params[:id])
+    @location.destroy
+
+    redirect_to location_path
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :type, :is_przypal, :address)
   end
 end
